5a7c88a0151ff189a40b1c61221836c5c464d386
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2023-08-19 16:29:31 -0700

    Update crawler.py
    Crawler.py automatically searches all W3Schools Python tutorials.

--- exampleScraper/crawler.py
+++ exampleScraper/crawler.py
@@ -1,3 +1,13 @@
 # Aug 19, 2023
 # W3 Schools Crawler
 
+import page_reader
+import requests
+from bs4 import BeautifulSoup
+
+home_page = requests.get("https://www.w3schools.com/python/")
+soup = BeautifulSoup(home_page.text, 'html.parser')
+
+# Visits all items in the left menu
+for item in soup.find("div",{"id":"leftmenuinnerinner"}).find_all("a", href=True):
+    print(page_reader.find_examples(f"https://www.w3schools.com/python/{item['href']}"))
