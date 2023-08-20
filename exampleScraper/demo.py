8d7e017afbea7eebdc163e8827b19df478f145de
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2023-08-20 15:11:07 -0700

    Add markdown to code format

--- exampleScraper/demo.py
+++ exampleScraper/demo.py
@@ -4,7 +4,7 @@ import py_examples
 def code_response(message, history):
     message = str(message)
     matching_lists = py_examples.search_similarity(message)
-    output = '\n\n'.join(['\n'.join(item) for item in matching_lists])
+    output = '\n\n'.join([f"{item[0]}\n```py\n{item[1]}\n```" for item in matching_lists])
     return output
 
 gr.ChatInterface(code_response).launch(share=True)
