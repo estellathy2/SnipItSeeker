4bb8c43b4cf3c695ffd44a430aefa24679d40f27
estellathy2 <98199976+estellathy2@users.noreply.github.com>
2023-08-20 16:05:16 -0400

    Update demo.py

--- exampleScraper/demo.py
+++ exampleScraper/demo.py
@@ -2,9 +2,13 @@ import gradio as gr
 import rw_json
 
 def code_response(message, history):
+    message = str(message)
+    matching_lists = []
     for i in rw_json.reading_json():
-        for j in i:
-            if j == message:
-                return i
+        if i and isinstance(i[0], str) and message in i[0]:
+            matching_lists.append(i)
 
-gr.ChatInterface(code_response).launch()
+    output = '\n\n'.join(['\n'.join(item) for item in matching_lists])
+    return output
+
+gr.ChatInterface(code_response).launch(share=True)
